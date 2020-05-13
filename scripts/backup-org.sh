2fb4e324e315e119e1695c62a02cd9534b9107bb
Chloe Lee <echloelee@gmail.com>
2020-05-13 20:52:02 +0800

    Include deleted files in commits

--- scripts/backup-org.sh
+++ scripts/backup-org.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd ~/Documents/Org
-git add *
+git add -u # include deleted files
 git commit -m "Automated backup"
 git push
--- scripts/backup-schoolwork.sh
+++ scripts/backup-schoolwork.sh
@@ -1,5 +1,5 @@
 cd ~/Documents/NUS/Y2S2
-git add *
+git add -u # include deleted files
 git add .gitignore
 git commit -m "Automated backup"
 git push

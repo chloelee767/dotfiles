b296e107061d9907054f0aad78032b1f20e75e5d
Chloe Lee <echloelee@gmail.com>
2020-05-13 15:34:34 +0800

    Org agenda and org mode changes

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -21,8 +21,15 @@
 (add-hook 'text-mode-hook 'turn-off-auto-fill)
 (remove-hook 'text-mode-hook #'auto-fill-mode)
 
-;; Org Todo and Agenda
+;; Org
 (after! org
+  (setq line-spacing 0.2)
+
+  ;; org ref
+  (setq org-ref-completion-library 'org-ref-ivy-cite)
+  (setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdf %f"))
+  (require 'org-ref)
+
   ;; org directory
   (setq org-files-directory "~/Documents/Org")
   (setq org-agenda-files (list org-files-directory))
@@ -34,31 +41,34 @@
   (setq org-agenda-start-on-weekday nil)
   (setq org-agenda-start-day "0d")
 
-  (setq org-deadline-warning-days 0) ;; don't clutter today with future deadlines
-  (setq org-agenda-skip-scheduled-if-deadline-is-shown t) ;; don't show scheduled and deadline on same day
-
   ;; don't show done items in agenda view
   (setq org-agenda-skip-deadline-if-done t)
   (setq org-agenda-skip-scheduled-if-done t)
 
-  ;; TODO is there a way to truncate the breadcrumbs? Or ideally display properties
-  (setq org-agenda-prefix-format '((agenda . " %i %-12:c %?-12t %s %-20b")
-                                   (todo . " %i %-12:c %-20b")
-                                   (tags . " %i %-12:c %-20b")
-                                   (search . " %i %-12:c %-20b")))
+  ;; Show headings in agenda
+  (setq org-agenda-prefix-format '((agenda . " %i %-12:c %?-12t %s %-12b")
+                                   (todo . " %i %-12:c %-12b")
+                                   (tags . " %i %-12:c %-12b")
+                                   (search . " %i %-12:c %-12b")))
 
-  (setq org-agenda-custom-commands '(("q" "Agenda with unscheduled items"
-                                      ((agenda "" ((org-agenda-span 12)))
-                                       (tags "-longterm+DEADLINE=\"\"+SCHEDULED=\"\"/!")
-                                       ))
-                                     ("u" "Unscheduled items"
-                                      ((tags "-longterm+DEADLINE=\"\"+SCHEDULED=\"\"/!"))
-                                      )
-                                     ("l" "Long term"
-                                      ((tags "+longterm/!")) ;; /! : match not done TODOs
-                                      )))
-
-  ;; (setq org-agenda-overriding-columns-format "%CATEGORY %LABEL %TODO %3PRIORITY %ITEM %DEADLINE %SCHEDULED %TAGS")
+  (setq org-agenda-custom-commands '(
+    ("s" "2 week agenda"
+     ((agenda "" (
+                  (org-agenda-span 14)
+                  (org-deadline-warning-days 0) ;; don't clutter with future deadlines
+                  ))))
+    ("q" "Daily agenda"
+     ((agenda "" (
+                  (org-agenda-span 1)
+                  (org-deadline-warning-days 31) ;; up to 1 month warning
+                  ))
+      (tags-todo "-longterm+DEADLINE=\"\"+SCHEDULED=\"\"") ;; unscheduled TOODs
+      ))
+    ("u" "Unscheduled TODOs"
+      ((tags-todo "-longterm+DEADLINE=\"\"+SCHEDULED=\"\"")))
+    ("l" "Long term, unscheduled TODOs"
+      ((tags-todo "+longterm+DEADLINE=\"\"+SCHEDULED=\"\"")))
+                                     ))
 )
 
 ;; Pretty symbols
@@ -71,3 +81,4 @@
                                       ("null"   . ?∅)
                                       ("in"     . ?∈)
                                       ("not in" . ?∉)))
+

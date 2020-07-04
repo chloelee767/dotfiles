14e2745f9baa09d9f7a3dc7d433bd9276d0d121d
Chloe <echloelee@gmail.com>
2020-07-04 15:27:45 +0800

    clean up config.el + minor improvements + fix theme related things

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -1,13 +1,11 @@
 ;;; .doom.d/config.el -*- lexical-binding: t; -*-
 
 ;;;; Appearance ;;;;
-;; TODO change font comment font colours
-(setq doom-theme 'doom-peacock)
-(setq doom-peacock-brighter-comments t)
+;; TODO set treemacs font to be the same as doom-font
+(setq doom-theme 'doom-peacock-v2)
 (setq doom-font (font-spec :family "Iosevka Nerd Font" :size 16))
 
 (global-prettify-symbols-mode 1)
-;; (toggle-frame-maximized)
 
 ;; Enable line wrapping
 (global-visual-line-mode 1)
@@ -19,47 +17,28 @@
 (remove-hook 'text-mode-hook #'auto-fill-mode)
 
 ;;;; Org ;;;;
-(after! org
-  ;; directories and files
-  (setq org-files-directory "~/Documents/Org/"
-        org-roam-directory (concat org-files-directory "notes/")
-        deft-directory org-roam-directory
-        deft-recursive t
-        cl/org-agenda-directory (concat org-files-directory "todos/")
-        org-roam-index-file (concat org-roam-directory "index.org") ;; access the index file using org-roam-jump-to-index
-        org-agenda-files (list cl/org-agenda-directory)
-        cl/org-refile-file (concat cl/org-agenda-directory "refile.org"))
+(setq org-files-directory "~/Documents/Org/"
+      org-roam-directory (concat org-files-directory "notes/")
+      deft-directory org-roam-directory
+      deft-recursive t
+      cl/org-agenda-directory (concat org-roam-directory "agenda/")
+      org-agenda-files (list cl/org-agenda-directory)
+      cl/org-refile-file (concat cl/org-agenda-directory "refile.org"))
 
+(after! org
+  (map! :map org-mode-map :localleader (:prefix "s" "y" #'org-copy-subtree))
+  ;; directories and files
   (setq org-list-allow-alphabetical t)
   (setq org-superstar-headline-bullets-list '("▶"))
   ;; (setq org-use-property-inheritance t)
-
   (setq org-todo-keywords
         '((sequence "TODO(t)" "PROJ(p)" "NEXT(n)" "|" "DONE(d!)")
           (sequence "WAIT(w)" "|" "KILL(k)")))
 
-  ;; org ref
-  (setq org-ref-completion-library 'org-ref-ivy-cite)
-  (setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdf %f"))
-  (require 'org-ref)
-
-  ;; org roam
-  (setq org-roam-capture-templates
-        '(("d" "default" plain (function org-roam--capture-get-point)
-           "%?"
-           :file-name "%<%Y%m%d%H%M%S>-${slug}"
-           :head "#+TITLE: ${title}\n"
-           :unnarrowed t)
-          ;; resources go to res/ folder
-          ("r" "resource" plain (function org-roam--capture-get-point)
-           :file-name "res/%<%Y%m%d%H%M%S>-${slug}"
-           :head "#+TITLE: ${title}\n"
-           :unnarrowed t)))
-  (setq org-roam-buffer-width 0.2)
-
   ;; org capture
   (setq org-capture-templates '(("t" "todo" entry (file cl/org-refile-file) "* TODO %?")))
 
+  (setq org-latex-pdf-process (list "latexmk -shell-escape -bibtex -f -pdf %f"))
   ;; agenda
   ;; make org agenda start on today
   (setq org-agenda-start-on-weekday nil)
@@ -94,18 +73,75 @@
   (setq org-agenda-dim-blocked-tasks nil) ;; so projects aren't dimmed
   )
 
+(use-package! org-ref
+  :after-call org-mode-hook
+  :config
+  (setq org-ref-completion-library 'org-ref-ivy-cite)
+  )
+
+(use-package! org-roam
+  :init
+  ;; keybindings
+  (map! :leader (:prefix "n" (:prefix "r"
+                              :desc "Open index file" "e" #'org-roam-jump-to-index
+                              :desc "org-mark-ring-goto" "m" #'org-mark-ring-goto
+                              )))
+  (map! :map org-mode-map
+        :localleader
+        (:prefix "m"
+         "e" #'org-roam-jump-to-index
+         "h" #'org-mark-ring-goto
+         ))
+  :config
+  (setq +org-roam-open-buffer-on-find-file nil)
+  (setq org-roam-tag-sources '(prop all-directories))
+  (setq org-roam-capture-templates
+        '(("d" "default" plain (function org-roam--capture-get-point)
+           "%?"
+           :file-name "%<%Y%m%d%H%M%S>-${slug}"
+           :head "#+TITLE: ${title}\n"
+           :unnarrowed t)
+
+          ("b" "book" plain (function org-roam--capture-get-point)
+           :file-name "res/books/%<%Y%m%d%H%M%S>-${slug}"
+           :head "#+TITLE: ${title}\n"
+           :unnarrowed t)
+
+          ("p" "paper" plain (function org-roam--capture-get-point)
+           :file-name "res/papers/%<%Y%m%d%H%M%S>-${slug}"
+           :head "#+TITLE: ${title}\n"
+           :unnarrowed t)
+
+          ("w" "website" plain (function org-roam--capture-get-point)
+           :file-name "res/websites/%<%Y%m%d%H%M%S>-${slug}"
+           :head "#+TITLE: ${title}\n"
+           :unnarrowed t)
+
+          ("r" "other resource" plain (function org-roam--capture-get-point)
+           :file-name "res/%<%Y%m%d%H%M%S>-${slug}"
+           :head "#+TITLE: ${title}\n"
+           :unnarrowed t)
+
+          ("e" "person" plain (function org-roam--capture-get-point)
+           :file-name "people/${slug}"
+           :head "#+TITLE: ${title}\n"
+           :unnarrowed t)
+          )
+        )
+  )
+
 ;;;; LSP ;;;;
 ;; use microsoft python lsp
 (after! lsp-python-ms
   (set-lsp-priority! 'mspyls 1))
 
-;; fix lsp showing outdated errors: https://github.com/hlissner/doom-emacs/issues/3267
-(setq flycheck-check-syntax-automatically '(save idle-change new-line mode-enabled))
-
 ;;;; Spellcheck ;;;;
-(setq ispell-dictionary "en_GB")
+(setq ispell-dictionary "british")
+;; ispell-word and flyspell-correct-at-point are both bound to z =
+;; HACK binding directly to the ispell-word function causes flyspell-correct-at-point to be called for some reason
+(map! :nv "z s" (lambda () (interactive)(ispell-word)))
 
-;;;; Keybindings ;;;;
+;;;; Misc ;;;;
 (map! :n "C-!" #'rotate-text-backward)
 
 (map!
@@ -121,8 +157,10 @@
  :nv "g^" #'evil-first-non-blank-of-line
  :nv "g$" #'evil-end-of-line
  )
-
 ;; looks interesting: https://github.com/hlissner/doom-emacs/blob/develop/docs/api.org#create-a-paste-transient-state-to-cycle-through-kill-ring-on-paste
 
+(map! :nv "C-w" #'ace-window)
+
+(setq delete-by-moving-to-trash t)
 
 (load! "private.el")
---
+++ doom/.doom.d/themes/doom-peacock-v2-theme.el
@@ -0,0 +1,207 @@
+;;; doom-peacock-v2-theme.el --- Doom's Peacock theme with minor tweaks -*- no-byte-compile: t; -*-
+(require 'doom-themes)
+
+(defgroup doom-peacock-v2-theme nil
+  "Options for doom-themes"
+  :group 'doom-themes)
+
+(defcustom doom-peacock-v2-brighter-modeline nil
+  "If non-nil, more vivid colors will be used to style the mode-line."
+  :group 'doom-peacock-v2-theme
+  :type 'boolean)
+
+(defcustom doom-peacock-v2-brighter-comments nil
+  "If non-nil, comments will be highlighted in more vivid colors."
+  :group 'doom-peacock-v2-theme
+  :type 'boolean)
+
+(defcustom doom-peacock-v2-comment-bg doom-peacock-v2-brighter-comments
+  "If non-nil, comments will have a subtle, darker background. Enhancing their
+legibility."
+  :group 'doom-peacock-v2-theme
+  :type 'boolean)
+
+(defcustom doom-peacock-v2-padded-modeline doom-themes-padded-modeline
+  "If non-nil, adds a 4px padding to the mode-line. Can be an integer to
+determine the exact padding."
+  :group 'doom-peacock-v2-theme
+  :type '(choice integer boolean))
+
+;;
+(def-doom-theme doom-peacock-v2
+  "Peacock theme from daylerees themes "
+
+  ;; darker background (taken from doom-monokai-classic)
+  ((bg         '("#272822" nil       nil          ))
+   (bg-alt     '("#1D1E19" nil       nil          ))
+   (base0      '("#1B2229" "black"   "black"      ))
+   (base1      '("#161613" "#101010" "brightblack"))
+   (base2      '("#1D1F20" "#191919" "brightblack"))
+   (base3      '("#2D2E2E" "#252525" "brightblack"))
+   (base4      '("#4E4E4E" "#454545" "brightblack"))
+   (base5      '("#555556" "#6B6B6B" "brightblack"))
+   (base6      '("#767679" "#7B7B7B" "brightblack"))
+   (base7      '("#CFC0C5" "#C1C1C1" "brightblack"))
+   (base8      '("#FFFFFF" "#FFFFFF" "brightwhite"))
+   (fg         '("#F8F8F2" "#DFDFDF" "brightwhite"))
+   (fg-alt     '("#556172" "#4D4D4D" "white"))
+
+   (grey       base4)
+   (white      '("#f8f8f0" "base4"   "base4"        ))
+   (red        '("#ff5d38" "#ff6655" "red"          )) ;; peacock todo 16
+   ;; (orange     '("#cb4b16" "#dd8844" "brightred"    ))
+   (orange     '("#FD971F" "#FD971F" "brightred")) ;; lighter
+   (green      '("#98be65" "#99bb66" "green"        ))
+   (teal       '("#26a6a6" "#44b9b1" "brightgreen"  )) ;; peacock
+   (yellow     '("#bcd42a" "#ECBE7B" "yellow"       )) ;; peacock, todo 16
+   (blue       '("#51afef" "#51afef" "brightblue"   ))
+   (dark-blue  '("#2257A0" "#2257A0" "blue"         ))
+   (magenta    '("#c678dd" "#c678dd" "magenta"      ))
+   (violet     '("#a9a1e1" "#a9a1e1" "brightmagenta"))
+   (cyan       '("#46D9FF" "#46D9FF" "brightcyan"   ))
+   (dark-cyan  '("#5699AF" "#5699AF" "cyan"         ))
+   (coral-popup  '("#a60033" "#f6bfbc" "coral-popup"         ))
+
+   ;; face categories -- required for all themes
+   (highlight      red)
+   (vertical-bar   (doom-lighten bg 0.1))
+   (selection      coral-popup)
+   (builtin        red)
+   (comments       (doom-darken fg 0.3)) ;; more visible comments which don't clash with existing colours
+   (doc-comments   (doom-darken fg 0.3))
+   ;; (comments       (if doom-peacock-v2-brighter-comments dark-cyan base5)) ;; TODO
+   ;; (doc-comments   (doom-lighten (if doom-peacock-v2-brighter-comments dark-cyan base5) 0.25)) ;; TODO
+   (constants      red)        ;; done
+   (functions      yellow)     ;; done
+   (keywords       teal)       ;; done
+   (methods        yellow)     ;; not sure how to test this.
+   (operators      red)        ;; not showing up on `=` etc.
+   (type           white)      ;;
+   (strings        yellow)
+   (variables      white)      ;; done
+   (numbers        red)        ;; done
+
+   (region         `(,(doom-lighten (car bg-alt) 0.15) ,@(doom-lighten (cdr base0) 0.35)))
+   (error          red)
+   (warning        yellow)
+   (success        green)
+   (vc-modified    orange)
+   (vc-added       green)
+   (vc-deleted     red)
+
+   ;; custom categories
+   (-modeline-bright doom-peacock-v2-brighter-modeline)
+   (-modeline-pad
+    (when doom-peacock-v2-padded-modeline
+      (if (integerp doom-peacock-v2-padded-modeline) doom-peacock-v2-padded-modeline 4)))
+
+   (modeline-fg     nil)
+   (modeline-fg-alt (doom-blend violet base4 (if -modeline-bright 0.5 0.2)))
+
+   (modeline-bg
+    (if -modeline-bright
+        (doom-darken bg 0.475)
+      `(,(doom-darken (car bg) 0.15) ,@(cdr base0))))
+   (modeline-bg-l
+    (if -modeline-bright
+        (doom-darken blue 0.45)
+      `(,(doom-darken (car bg-alt) 0.1) ,@(cdr base0))))
+   (modeline-bg-inactive   (doom-darken bg 0.1))
+   (modeline-bg-inactive-l `(,(car bg) ,@(cdr base1))))
+
+
+  ;; --- extra faces ------------------------
+  ((elscreen-tab-other-screen-face :background "#353a42" :foreground "#1e2022")
+
+   (font-lock-comment-face
+    :foreground comments
+    :background (if doom-peacock-v2-comment-bg (doom-lighten bg 0.05))
+    :slant 'italic)
+   (font-lock-doc-face
+    :inherit 'font-lock-comment-face
+    :foreground doc-comments
+    :slant 'italic
+    :weight 'bold)
+
+   ((line-number &override) :foreground base4)
+   ((line-number-current-line &override) :foreground base7)
+
+   ;; tooltip
+   (tooltip              :background bg-alt :foreground fg)
+
+   ;; company
+   (company-tooltip            :inherit 'tooltip)
+   (company-tooltip-common                           :foreground highlight)
+   (company-tooltip-search     :background highlight :foreground bg :distant-foreground fg)
+   (company-tooltip-selection  :background selection)
+   (company-tooltip-mouse      :background magenta   :foreground bg :distant-foreground fg)
+   (company-tooltip-annotation                       :foreground violet)
+   (company-scrollbar-bg       :inherit 'tooltip)
+   (company-scrollbar-fg       :background highlight)
+   (company-preview                                  :foreground highlight)
+   (company-preview-common     :background base3 :foreground magenta)
+   (company-preview-search     :inherit 'company-tooltip-search)
+   (company-template-field     :inherit 'match)
+
+   ;; popup
+   (popup-face :inherit 'tooltip)
+   (popup-selection-face :inherit 'tooltip)
+
+   ;; pos-tip
+   (popup          :inherit 'tooltip)
+   (popup-tip-face :inherit 'tooltip)
+
+
+   (doom-modeline-bar :background (if -modeline-bright modeline-bg highlight))
+
+   (mode-line
+    :background modeline-bg :foreground modeline-fg
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg)))
+   (mode-line-inactive
+    :background modeline-bg-inactive :foreground modeline-fg-alt
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive)))
+   (mode-line-emphasis
+    :foreground (if -modeline-bright base8 highlight))
+
+   (solaire-mode-line-face
+    :inherit 'mode-line
+    :background modeline-bg-l
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-l)))
+   (solaire-mode-line-inactive-face
+    :inherit 'mode-line-inactive
+    :background modeline-bg-inactive-l
+    :box (if -modeline-pad `(:line-width ,-modeline-pad :color ,modeline-bg-inactive-l)))
+
+   ;; --- major-mode faces -------------------
+   ;; css-mode / scss-mode
+   (css-proprietary-property :foreground orange)
+   (css-property             :foreground green)
+   (css-selector             :foreground blue)
+
+   ;; markdown-mode
+   (markdown-markup-face :foreground base5)
+   (markdown-header-face :inherit 'bold :foreground red)
+   ((markdown-code-face &override) :background (doom-lighten base3 0.05))
+
+   ;; rjsx-mode
+   (rjsx-tag :foreground teal)
+   (rjsx-attr :foreground red)
+
+   ;; org-mode
+   (org-level-1 :foreground teal :weight 'bold)
+   (org-level-2 :foreground orange :weight 'bold)
+   (org-level-3 :foreground violet :weight 'bold)
+   (org-level-4 :foreground red :weight 'bold)
+   (org-level-5 :foreground green :weight 'bold)
+   (org-level-6 :foreground cyan :weight 'bold)
+   (org-level-7 :foreground blue :weight 'bold)
+   (org-level-8 :foreground yellow :weight 'bold)
+   (org-link :foreground red :underline t)
+   )
+
+
+  ;; --- extra variables ---------------------
+  ;; ()
+  )
+
+;;; doom-peacock-v2-theme.el ends here

764f7ea81079a3e2c8fc714cad362f8a443f4b29
chloelee767 <echloelee@gmail.com>
2021-06-23 09:29:32 +0800

    Fix org roam v2

--- doom/.doom.d/org-config.el
+++ doom/.doom.d/org-config.el
@@ -81,28 +81,76 @@
 ;;
 ;;; Note taking
 
+(defun chloe/org-roam-file-slug (input)
+  "My version of `org-roam-node-slug'
+- Replace spaces with -
+- Downcase
+- Ensures no trailing or sequential underscores"
+  ;; Adapted from `org-roam-node-slug'
+  (let ((slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
+                           768 ; U+0300 COMBINING GRAVE ACCENT
+                           769 ; U+0301 COMBINING ACUTE ACCENT
+                           770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
+                           771 ; U+0303 COMBINING TILDE
+                           772 ; U+0304 COMBINING MACRON
+                           774 ; U+0306 COMBINING BREVE
+                           775 ; U+0307 COMBINING DOT ABOVE
+                           776 ; U+0308 COMBINING DIAERESIS
+                           777 ; U+0309 COMBINING HOOK ABOVE
+                           778 ; U+030A COMBINING RING ABOVE
+                           780 ; U+030C COMBINING CARON
+                           795 ; U+031B COMBINING HORN
+                           803 ; U+0323 COMBINING DOT BELOW
+                           804 ; U+0324 COMBINING DIAERESIS BELOW
+                           805 ; U+0325 COMBINING RING BELOW
+                           807 ; U+0327 COMBINING CEDILLA
+                           813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
+                           814 ; U+032E COMBINING BREVE BELOW
+                           816 ; U+0330 COMBINING TILDE BELOW
+                           817 ; U+0331 COMBINING MACRON BELOW
+                           )))
+    (cl-flet* ((nonspacing-mark-p (char)
+                                  (memq char slug-trim-chars))
+               (strip-nonspacing-marks (s)
+                                       (ucs-normalize-NFC-string
+                                        (apply #'string (seq-remove #'nonspacing-mark-p
+                                                                    (ucs-normalize-NFD-string s)))))
+               (cl-replace (title pair)
+                           (replace-regexp-in-string (car pair) (cdr pair) title)))
+      (let* ((pairs `(("[^[:alnum:][:digit:].]" . "-")  ;; convert anything not alphanumeric or .
+                      ("--*" . "-")  ;; remove sequential dashes
+                      ("^-" . "")  ;; remove starting dash
+                      ("-$" . ""))) ;; remove ending dash
+             (slug (-reduce-from #'cl-replace (strip-nonspacing-marks input) pairs)))
+        (downcase slug)))))
+
+(defun chloe/org-roam-title (slug)
+  (file-name-extension slug))
 
 (use-package! org-roam
   :after org
-  :commands
-  (org-roam-buffer
-   org-roam-setup
-   org-roam-capture
-   org-roam-node-find)
-  :config
+  :init
   (map! :after org
         :leader
         :prefix ("n" . "notes")
+        :desc "Org roam find" "f" #'org-roam-node-find
         :desc "Org roam buffer" "r" #'org-roam-buffer-toggle)
+  :config
   ;;(setq org-roam-mode-sections
   ;;      (list #'org-roam-backlinks-insert-section
   ;;            #'org-roam-reflinks-insert-section
   ;;            #'org-roam-unlinked-references-insert-section))
-  ;; (setq org-roam-db-location (concat doom-private-dir "local/org-roam.db"))
-        (setq org-roam-db-location "~/org-roam.db")
-  (org-roam-setup))
-
-
+  (org-roam-setup)
+  (map! :map org-mode-map
+        :i "C-c i" #'org-roam-node-insert)
+  (setq org-roam-db-location "~/org-roam.db"
+        org-roam-node-display-template "${file:*}${olp:*}"
+        org-roam-capture-templates
+        '(("d" "default" plain "%?"
+           :if-new (file+head "%(chloe/org-roam-file-slug \"${title}\").org"
+                              "#+title: %(chloe/org-roam-title (chloe/org-roam-file-slug \"${title}\"))")
+           :immediate-finish t
+           :unnarrowed t))))
 
 ;; (use-package! org-roam
 ;;   :hook (org-load . org-roam-mode)

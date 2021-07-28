717f209ea48f25fa2b4650c21d23001be4f4efc5
chloelee767 <echloelee@gmail.com>
2021-07-28 23:01:05 +0800

    emacs: go generate, fixes

--- doom/.doom.d/modules/lang/go/autoload.el
+++ doom/.doom.d/modules/lang/go/autoload.el
@@ -55,6 +55,8 @@
         (+go--run-tests (concat "-test.run=NONE -test.bench" "='" (match-string-no-properties 2) "'")))
     (error "Must be in a _test.go file")))
 
+;;
+;;; Run
 
 ;;;###autoload
 (defun +go/play-buffer-or-region (&optional beg end)
@@ -63,3 +65,62 @@
   (if (use-region-p)
       (go-play-region beg end)
     (go-play-buffer)))
+
+;;
+;;; go generate
+
+(defun +go--generate (dir args)
+  (let ((cd-cmd (concat "cd " dir))
+        (generate-cmd (concat "go generate " args)))
+    (+go--spawn (concat cd-cmd " && " generate-cmd))))
+
+;;;###autoload
+(defun +go/generate-file ()
+  "Run go generate for the current file only."
+  (interactive)
+  (if buffer-file-name
+      (+go--generate default-directory (file-name-nondirectory buffer-file-name))
+    (error "Couldn't get filename of curent buffer")))
+
+;;;###autoload
+(defun +go/generate-dir ()
+  "Run go generate for the current directory recursively."
+  (interactive)
+  (+go--generate default-directory "./..."))
+
+;;;###autoload
+(defun +go/generate-project ()
+  "Run go generate for the entire project."
+  (interactive)
+  ;; interactively choose project root if we cannot find it
+  (if-let ((project (project-current t nil)))
+      (+go--generate (file-truename (project-root project)) "./..."))
+  (error "Couldn't get current project."))
+
+;;;###autoload
+(defun +go/generate-line ()
+  "Run go generate for just the line at the current point."
+  (interactive)
+  (let ((curr-line-string (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
+        (regexp (rx line-start "//go:generate")))
+    (cond ((string-match "\"" curr-line-string)
+           (error "go generate cannot handle quotes (I think)"))
+
+          ((string-match regexp curr-line-string)
+           (let ((run-opt (concat "-run=\"" curr-line-string "\""))
+                 (filename (file-name-nondirectory buffer-file-name)))
+             (+go--generate default-directory (concat run-opt " " filename))))
+
+          (t (error "Line does not begin with //go:generate")))))
+
+
+;; doesn't exactly behave like go generate (go generate CLI doesn't always escape quotes properly)
+;;;###autoload
+(defun +go/generate-line-simple ()
+  "Run go generate for just the line at the current point."
+  (interactive)
+  (let* ((curr-line-string (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
+         (cmd (string-trim-left curr-line-string "//go:generate ")))
+    (if (not (eq curr-line-string cmd))
+        (+go--spawn cmd)
+      (error "Line does not begin with //go:generate"))))
--- doom/.doom.d/modules/lang/go/config.el
+++ doom/.doom.d/modules/lang/go/config.el
@@ -6,10 +6,11 @@
 (after! go-mode
   (set-docsets! 'go-mode "Go")
   (set-repl-handler! 'go-mode #'gorepl-run)
-  ;; (set-lookup-handlers! 'go-mode
-  ;;   :definition #'go-guru-definition
-  ;;   :references #'go-guru-referrers
-  ;;   :documentation #'godoc-at-point)
+  (unless (featurep! +lsp)
+    (set-lookup-handlers! 'go-mode
+      :definition #'go-guru-definition
+      :references #'go-guru-referrers
+      :documentation #'godoc-at-point))
 
   ;; Redefines default formatter to *not* use goimports if reformatting a
   ;; region; as it doesn't play well with partial code.
@@ -30,35 +31,42 @@
         "e" #'+go/play-buffer-or-region
         "i" #'go-goto-imports      ; Go to imports
         (:prefix ("h" . "help")
-          "." #'godoc-at-point)     ; Lookup in godoc
-        ;;   "d" #'go-guru-describe   ; Describe this
-        ;;   "v" #'go-guru-freevars   ; List free variables
-        ;;   "i" #'go-guru-implements ; Implements relations for package types
-        ;;   "p" #'go-guru-peers      ; List peers for channel
-        ;;   "P" #'go-guru-pointsto   ; What does this point to
-        ;;   "r" #'go-guru-referrers  ; List references to object
-        ;;   "e" #'go-guru-whicherrs  ; Which errors
-        ;;   "w" #'go-guru-what       ; What query
-        ;;   "c" #'go-guru-callers    ; Show callers of this function
-        ;;   "C" #'go-guru-callees)   ; Show callees of this function
+         "." #'godoc-at-point ; Lookup in godoc
+         (:unless (featurep! +lsp)
+          "d" #'go-guru-describe   ; Describe this
+          "v" #'go-guru-freevars   ; List free variables
+          "i" #'go-guru-implements ; Implements relations for package types
+          "p" #'go-guru-peers      ; List peers for channel
+          "P" #'go-guru-pointsto   ; What does this point to
+          "r" #'go-guru-referrers  ; List references to object
+          "e" #'go-guru-whicherrs  ; Which errors
+          "w" #'go-guru-what       ; What query
+          "c" #'go-guru-callers    ; Show callers of this function
+          "C" #'go-guru-callees   ; Show callees of this function
+          ))
         (:prefix ("ri" . "imports")
-          "a" #'go-import-add
-          "r" #'go-remove-unused-imports)
+         "a" #'go-import-add
+         "r" #'go-remove-unused-imports)
         (:prefix ("b" . "build")
-          :desc "go run ." "r" (cmd! (compile "go run ."))
-          :desc "go build" "b" (cmd! (compile "go build"))
-          :desc "go clean" "c" (cmd! (compile "go clean")))
+         :desc "go run ." "r" (cmd! (compile "go run ."))
+         :desc "go build" "b" (cmd! (compile "go build"))
+         :desc "go clean" "c" (cmd! (compile "go clean")))
         (:prefix ("t" . "test")
-          "t" #'+go/test-rerun
-          "a" #'+go/test-all
-          "s" #'+go/test-single
-          "n" #'+go/test-nested
-          "g" #'go-gen-test-dwim
-          "G" #'go-gen-test-all
-          "e" #'go-gen-test-exported
-          (:prefix ("b" . "bench")
-            "s" #'+go/bench-single
-            "a" #'+go/bench-all))))
+         "t" #'+go/test-rerun
+         "a" #'+go/test-all
+         "s" #'+go/test-single
+         "n" #'+go/test-nested
+         "g" #'go-gen-test-dwim
+         "G" #'go-gen-test-all
+         "e" #'go-gen-test-exported
+         (:prefix ("b" . "bench")
+          "s" #'+go/bench-single
+          "a" #'+go/bench-all)
+         (:prefix ("g" . "generate")
+          "f" #'+go/generate-file
+          "l" #'+go/generate-line
+          "d" #'+go/generate-dir
+          "p" #'+go/generate-project))))
 
 
 (use-package! gorepl-mode

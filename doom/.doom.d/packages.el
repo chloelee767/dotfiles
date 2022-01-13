5ff33e705362fa144470ad29107ff67954eb25e4
chloelee767 <echloelee@gmail.com>
2022-01-13 09:55:37 +0800

    Update packages

--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -7,7 +7,8 @@
 ;; (package! builtin-package :disable t)
 
 (package! org-roam
-  :pin "de47f0a28d0e346c88c632ea1bb078b69d1e4b52")
+  :recipe (:host github :repo "org-roam/org-roam")
+  :pin "abe63b436035049923ae96639b9b856697047779")
 ;; (package! org-roam :pin "78a371cdc4facf58fb20b513eaf60b24f3aaa7f7")
 (package! org-fragtog :pin "92119e3ae7c9a0ae2b5c9d9e4801b5fdc4804ad7")
 (package! org-superstar :pin "94f35c20f8b84a63defa145e3e6ae735fa33dd5d")

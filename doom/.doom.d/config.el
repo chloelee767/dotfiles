30a86b4e39e8e94db7a8832e6794f787f09a3f17
chloelee767 <echloelee@gmail.com>
2022-10-16 11:45:07 +0800

    Emacs: org roam: fix titles and naming of notes created

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -282,51 +282,16 @@
   ;; org-download-image-dir is safe as long as it is a string
   (put 'org-download-image-dir 'safe-local-variable #'stringp))
 
-(defun chloe/org-roam-file-slug (input)
-  "My version of `org-roam-node-slug'
-- Replace spaces with -
-- Downcase
-- Ensures no trailing or sequential underscores"
-  ;; Adapted from `org-roam-node-slug'
-  (let ((slug-trim-chars '(;; Combining Diacritical Marks https://www.unicode.org/charts/PDF/U0300.pdf
-                           768 ; U+0300 COMBINING GRAVE ACCENT
-                           769 ; U+0301 COMBINING ACUTE ACCENT
-                           770 ; U+0302 COMBINING CIRCUMFLEX ACCENT
-                           771 ; U+0303 COMBINING TILDE
-                           772 ; U+0304 COMBINING MACRON
-                           774 ; U+0306 COMBINING BREVE
-                           775 ; U+0307 COMBINING DOT ABOVE
-                           776 ; U+0308 COMBINING DIAERESIS
-                           777 ; U+0309 COMBINING HOOK ABOVE
-                           778 ; U+030A COMBINING RING ABOVE
-                           780 ; U+030C COMBINING CARON
-                           795 ; U+031B COMBINING HORN
-                           803 ; U+0323 COMBINING DOT BELOW
-                           804 ; U+0324 COMBINING DIAERESIS BELOW
-                           805 ; U+0325 COMBINING RING BELOW
-                           807 ; U+0327 COMBINING CEDILLA
-                           813 ; U+032D COMBINING CIRCUMFLEX ACCENT BELOW
-                           814 ; U+032E COMBINING BREVE BELOW
-                           816 ; U+0330 COMBINING TILDE BELOW
-                           817 ; U+0331 COMBINING MACRON BELOW
-                           )))
-    (cl-flet* ((nonspacing-mark-p (char)
-                                  (memq char slug-trim-chars))
-               (strip-nonspacing-marks (s)
-                                       (ucs-normalize-NFC-string
-                                        (apply #'string (seq-remove #'nonspacing-mark-p
-                                                                    (ucs-normalize-NFD-string s)))))
-               (cl-replace (title pair)
-                           (replace-regexp-in-string (car pair) (cdr pair) title)))
-      (let* ((pairs `(("[^[:alnum:][:digit:].]" . "-")  ;; convert anything not alphanumeric or .
-                      ("--*" . "-")  ;; remove sequential dashes
-                      ("^-" . "")  ;; remove starting dash
-                      ("-$" . ""))) ;; remove ending dash
-             (slug (-reduce-from #'cl-replace (strip-nonspacing-marks input) pairs)))
-        (downcase slug)))))
+(defun chloe/org-roam-input-title-to-file-name (input)
+  ;; ref.An Interesting Article => ref.an-interesting-article
+  (downcase (string-replace " " "-" input)))
 
-(defun chloe/org-roam-title (slug)
-  (if (string-match "\\." slug) (file-name-extension slug) slug))
+(defun chloe/org-roam-input-title-to-file-title (input)
+  ;; ref.An Interesting Article => An Interesting Article
+  ;; ref => ref
+  (if (string-match "\\." input)
+      (file-name-extension input)
+    input)) ;; case when top level
 
 (use-package! org-roam
   :after org
@@ -344,12 +309,12 @@
 
   (cl-defmethod org-roam-node-my-title ((node org-roam-node))
     (file-name-sans-extension (file-relative-name (org-roam-node-file node) org-roam-directory)))
-  (setq org-roam-node-display-template "${my-title:*}${olp:*}")
   (setq org-roam-db-location "~/org-roam.db"
+        org-roam-node-display-template "${my-title:*}${olp:*}" ;; OLP = outline path
         org-roam-capture-templates
         '(("d" "default" plain "%?"
-           :if-new (file+head "%(chloe/org-roam-file-slug \"${title}\").org"
-                              "#+title: %(chloe/org-roam-title (chloe/org-roam-file-slug \"${title}\"))")
+           :if-new (file+head "%(chloe/org-roam-input-title-to-file-name \"${title}\").org"
+                              "%(chloe/org-roam-input-title-to-file-title \"${title}\")")
            :immediate-finish t
            :unnarrowed t))
         org-roam-mode-sections (list #'org-roam-backlinks-insert-section

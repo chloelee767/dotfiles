6bb6cb13091dde29696be8790ada83c872f1cd50
chloelee767 <echloelee@gmail.com>
2024-01-25 19:56:19 +0800

    Emacs: update theme

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -93,7 +93,7 @@
 
 (setq
  ;; doom-theme (if (member (string-to-number (substring (current-time-string) 11 13)) (number-sequence 7 17)) 'doom-tomorrow-day 'doom-tomorrow-night) ;; set theme based on time
- doom-theme 'doom-nord-aurora
+ doom-theme 'doom-gruvbox
  doom-font (font-spec :family "Iosevka SS14" :size (if IS-MAC 13.0 11.0))
  doom-variable-pitch-font doom-font
  doom-serif-font (font-spec :family "Noto Serif")

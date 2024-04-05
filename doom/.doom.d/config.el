2d38c7dbf2f57ceca2b2b54bd80f05f43fbfc59b
chloelee767 <echloelee@gmail.com>
2024-04-05 09:36:52 +0800

    Emacs: add function to cleanup go imports

--- doom/.doom.d/config.el
+++ doom/.doom.d/config.el
@@ -264,7 +264,21 @@
   (setq gofmt-command "goimports"
         lsp-go-goimports-local "github.com/carousell"
         gofmt-args (append gofmt-args '("-local" "github.com/carousell")))
-  (add-hook 'before-save-hook 'gofmt-before-save))
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (defun chloe/go-cleanup-imports ()
+    (interactive)
+    (save-excursion
+      (go-goto-imports)
+      ;; remove blank lines before gofmt
+      (evil-command-window-ex-execute
+       (concat "1,"
+               (int-to-string (line-number-at-pos (point) t))
+               "s/\n\n+/\n"))
+      (gofmt)
+      (save-buffer)))
+  (map! :map go-mode-map
+        :localleader
+        (:prefix "ri" "c" #'chloe/go-cleanup-imports)))
 
 
 (setq lsp-clients-clangd-args '("-j=3"

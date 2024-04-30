ded12ed0d45a477fe824735b5ba60c495eb634c4
chloelee767 <echloelee@gmail.com>
2024-04-30 15:58:13 +0800

    emacs: remove org-roam stuff

--- doom/.doom.d/autoload.el
+++ doom/.doom.d/autoload.el
@@ -1,16 +1,5 @@
 ;;; ../dotfiles/doom/.doom.d/autoload.el -*- lexical-binding: t; -*-
 
-;; ;;;###autoload
-;; (defun chloe/toggle-org-roam-buffer-position ()
-;;   (interactive)
-;;   (progn
-;;   (setq org-roam-buffer-position (if (equal org-roam-buffer-position 'right) 'bottom 'right))
-;;     (when (eq (org-roam-buffer--visibility) 'visible)
-;;       (progn
-;;         (org-roam-buffer-toggle-display)
-;;         (org-roam-buffer-toggle-display)
-;;         ))))
-
 ;;;###autoload
 (defun chloe/set-file-local-org-download-dir ()
   "Still need to manually refresh the file local variables afterwards."
@@ -68,62 +57,3 @@
   (interactive "P")
   (+vertico/project-search arg (thing-at-point 'symbol)))
 
-;;;###autoload
-(defun chloe/org-roam-input-title-to-file-name (input)
-  ;; ref.An Interesting Article => ref.an-interesting-article
-  (downcase (string-replace
-             "/" "-"
-             (string-replace
-              "^" ""
-              (string-replace
-               "\\." "."
-               (string-replace
-                " " "-"
-                input))))))
-
-;;;###autoload
-(defun chloe/org-roam-input-title-to-file-title (input)
-  ;; ref.An Interesting Article => An Interesting Article
-  ;; ref => ref
-  (if (string-match "\\." input)
-      (file-name-extension input)
-    input)) ;; case when top level
-
-;;;###autoload (autoload 'org-roam-node-my-title "autoload" nil t)
-(cl-defmethod org-roam-node-my-title ((node org-roam-node))
-  (file-name-sans-extension (file-relative-name (org-roam-node-file node) org-roam-directory)))
-
-;;;###autoload
-(defun chloe/org-roam-node-find-children (&optional other-window)
-  ;; org-roam-node-find with intial input set so that only children of the current file are listed
-  (interactive current-prefix-arg)
-  (if (org-roam-file-p buffer-file-name)
-      (let ((note-name (file-name-sans-extension (file-name-nondirectory buffer-file-name))))
-        (org-roam-node-find other-window (concat "^" note-name "\\.")))
-    (error "Not in an org roam file")))
-
-;;;###autoload
-(defun chloe/org-roam-node-find-siblings ()
-  (interactive)
-  (if (org-roam-file-p buffer-file-name)
-      (let* ((notename (file-name-sans-extension (file-name-nondirectory buffer-file-name)))
-             (parent-notename (chloe/org-roam-note-parent notename)))
-        (if parent-notename
-            (org-roam-node-find nil (concat "^" parent-notename "\\."))
-          (message "Root level note, no parent")))
-    (error "Not in an org roam file")))
-
-;;;###autoload
-(defun chloe/org-roam-node-find-parent ()
-  (interactive)
-  (if (org-roam-file-p buffer-file-name)
-      (let* ((notename (file-name-sans-extension (file-name-nondirectory buffer-file-name)))
-             (parent-notename (chloe/org-roam-note-parent notename)))
-        (if parent-notename
-            (org-roam-node-find nil (concat "^" parent-notename))
-          (message "Root level note, no parent")))
-    (error "Not in an org roam file")))
-
-;; TODO replace all . with \\.
-;; TODO automatically visit if there is exactly 1 match in org-roam-node-find
-;; TODO more intelligent collision detection -- overwrite empty files
--- doom/.doom.d/packages.el
+++ doom/.doom.d/packages.el
@@ -6,10 +6,6 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
-(package! org-roam
-  :recipe (:host github :repo "org-roam/org-roam")
-  :pin "d95d25615e69e7cc847641800c1886366336c97e")
-;; (package! org-roam :pin "78a371cdc4facf58fb20b513eaf60b24f3aaa7f7")
 (package! org-fragtog :pin "92119e3ae7c9a0ae2b5c9d9e4801b5fdc4804ad7")
 (package! org-superstar :pin "94f35c20f8b84a63defa145e3e6ae735fa33dd5d")
 (package! org-download :pin "42ac361ef5502017e6fc1bceb00333eba90402f4")

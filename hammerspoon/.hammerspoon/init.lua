31a1c42f6329ebeb26d660bb8e6cc83d2f0942cf
chloelee767 <echloelee@gmail.com>
2025-03-03 22:58:42 +0800

    hammerspoon: add more shortcuts

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -16,4 +16,19 @@ spoon.MiroWindowsManager:bindHotkeys({
 -- hs.hotkey.bind('alt', 'tab', 'Next window', function()switcher_space:next()end)
 -- hs.hotkey.bind('alt-shift', 'tab', 'Prev window', function()switcher_space:previous()end)
 
+-- function openOrSwitch(app)
+--   local windows = hs.window.filter.new():setAppFilter(app):getWindows(hs.window.filter.sortByFocusedLast)
+--   local focusedWin = hs.window.focusedWindow()
+--   for _, win in ipairs(windows) do
+--     if win != focusedWin then
+--       win:focus()
+--       return
+--     end
+--   end
+-- end
+
 hs.hotkey.bind(hyper, "s", function() hs.application.open("Slack") end)
+hs.hotkey.bind(hyper, "t", function() hs.application.open("iTerm") end)
+hs.hotkey.bind(hyper, "n", function() hs.application.open("TriliumNext Notes") end)
+hs.hotkey.bind(hyper, "w", function() hs.application.open("Google Chrome") end)
+hs.hotkey.bind(hyper, "e", function() hs.application.open("Emacs") end)

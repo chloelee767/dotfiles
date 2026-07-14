39e2b8a529dbb77f4411ad4855a69aebd8409d38
chloelee767 <echloelee@gmail.com>
2026-07-14 17:55:13 +0800

    feat: apply local LazyVim customizations
    Custom keymaps (path/line yanking, buffer shortcuts, restored native
    H/L), wrap/linebreak options, sleuth.lua plugin, markdown customisations
    and lockfile updates.

--- justfile
+++ justfile
@@ -1,5 +1,5 @@
 work:
-    ./sync-dir.sh general shell hammerspoon work kubernetes doom neovim ai ghostty zellij
+    ./sync-dir.sh general shell hammerspoon work kubernetes doom neovim ai ghostty zellij lazyvim
 
 test:
     python3 -m unittest discover tests
--- lazyvim/.config/lazyvim/lua/config/keymaps.lua
+++ lazyvim/.config/lazyvim/lua/config/keymaps.lua
@@ -1,3 +1,63 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Restore native H/L (top/bottom of window) by removing LazyVim's
+-- bufferline prev/next buffer keymaps
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+
+-- buffers
+vim.keymap.set("n", "<leader>bk", function()
+  Snacks.bufdelete()
+end, { desc = "Delete Buffer" })
+
+vim.keymap.set("n", "<leader>px", function()
+  Snacks.scratch()
+end, { desc = "Toggle Scratch Buffer" })
+
+vim.keymap.set("n", "<leader>bS", "<cmd>wa<cr>", { desc = "Save All Files" })
+
+-- replace default "Delete Other Buffers" keymap (<leader>bo) with <leader>bO
+vim.keymap.del("n", "<leader>bo")
+vim.keymap.set("n", "<leader>bO", function()
+  Snacks.bufdelete.other()
+end, { desc = "Delete Other Buffers" })
+
+-- files
+vim.keymap.set("n", "<leader>fs", "<cmd>w<cr>", { desc = "Save File" })
+
+-- copy file path with line number(s) to clipboard
+local function yank_path_with_lines(modifier)
+  return function()
+    local path = vim.fn.expand(modifier)
+    local mode = vim.fn.mode()
+    local line_ref
+    if mode == "v" or mode == "V" or mode == "\22" then
+      local l1, l2 = vim.fn.line("v"), vim.fn.line(".")
+      if l1 > l2 then
+        l1, l2 = l2, l1
+      end
+      line_ref = l1 == l2 and tostring(l1) or (l1 .. "-" .. l2)
+      vim.cmd("normal! \27") -- exit visual mode
+    else
+      line_ref = tostring(vim.fn.line("."))
+    end
+    local result = path .. ":" .. line_ref
+    vim.fn.setreg("+", result)
+    vim.notify("Copied: " .. result)
+  end
+end
+
+vim.keymap.set(
+  { "n", "v" },
+  "<leader>fy",
+  yank_path_with_lines("%:p"),
+  { desc = "Copy Absolute Path:Line to Clipboard" }
+)
+vim.keymap.set(
+  { "n", "v" },
+  "<leader>fY",
+  yank_path_with_lines("%:."),
+  { desc = "Copy Relative Path:Line to Clipboard" }
+)
